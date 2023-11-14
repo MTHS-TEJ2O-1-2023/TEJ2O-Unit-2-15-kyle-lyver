15680c2f6307e9a49a3fb890f302a9c16fbbfc23
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-14 18:24:47 -0500

    read me fixed and comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Nov 2023
+ * This program moves a pixel around the microbit
 */
 
 basic.showString('Hello, World!')
