054eba5fcd3872a3bdc732c5ffa8545de3e9f065
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-23 14:38:58 +0000

    pythony

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,66 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Kyle Lyver
+Created on: Nov 2023
+This module is a Micro:bit MicroPython program that moves a pixel around the edge.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# variables
+y_axis_coordiante = 0
+x_axis_coordiante = 0
+
+# setup
+display.clear()
+display.show(Image.DUCK)
+sleep(1000)
+
+# loop
+while True:
+    if button_a.is_pressed():
+        # setup
+        display.clear()
+
+        # reset variables
+        y_axis_coordiante = 0
+        x_axis_coordiante = 0
+
+        # loop that moves pixel down
+        while y_axis_coordiante <= 3:
+            # loop that moves pixel right
+            while x_axis_coordiante <= 3:
+                sleep(500)
+
+                display.set_pixel(x_axis_coordiante, y_axis_coordiante, 9)
+                sleep(500)
+                display.clear()
+
+                x_axis_coordiante += 1
+            sleep(500)
+            display.clear()
+            display.set_pixel(x_axis_coordiante, y_axis_coordiante, 9)
+            y_axis_coordiante += 1
+        # delete pixel after it reaches (4, 3)
+        display.clear()
+
+        # loop that moves pixel up
+        while y_axis_coordiante >= 0:
+            # loop that moves pixel left
+            while x_axis_coordiante >= 1:
+                sleep(500)
+
+                display.set_pixel(x_axis_coordiante, y_axis_coordiante, 9)
+                sleep(500)
+                display.clear()
+
+                x_axis_coordiante -= 1
+
+            sleep(500)
+            display.clear()
+            display.set_pixel(x_axis_coordiante, y_axis_coordiante, 9)
+            y_axis_coordiante -= 1
+        # when full turn is completed
+        sleep(500)
+        display.clear()
+        display.show(Image.DUCK)
